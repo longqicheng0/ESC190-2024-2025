b56c422ba4beeaf3e97c30879fe198c2f7435bf8
longqicheng0 <143105215+longqicheng0@users.noreply.github.com>
2025-05-04 12:48:07 -0400

    Delete Midterm-Finals Review/heapq.py

--- Midterm-Finals Review/heapq.py
+++
@@ -1,9 +0,0 @@
-import heapq
-
-list_needto_Modify = [1,2,3,4,5]
-
-heapq.heapify(list_needto_Modify)
-heapq.heappush(list_needto_Modify,(priority,item)) #note this is from lowest to hightest number
-# so you might need
-list_needto_Modify.sort(reverse=True)
-heapq.heappop(list_needto_Modify)
